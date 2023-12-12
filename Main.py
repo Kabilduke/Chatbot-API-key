0169cc0e9323bfe7da7563dd9cd90118725e8c67
K A B I L <100506503+Kabilduke@users.noreply.github.com>
2023-12-12 09:51:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,8 @@ def chat_with_gpt(prompt, max_tokens= 30):
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages =[
-            {"role": "system", "content": "recommed music according to moods"}, 
+            # Change the role and content ticket booking assistant, Language translator...etc
+            {"role": "system", "content": "ticket booking assistant"}, 
             {"role":"user", "content": prompt}
             ],
         max_tokens= max_tokens
@@ -15,7 +16,7 @@ def chat_with_gpt(prompt, max_tokens= 30):
     return response.choices[0].message.content.strip()
 
 if __name__ == "__main__":
-    # print("Bot: Hi! I'm a ticket booking assistant. You can ask me about available flights, prices, and booking details.")
+    print("Bot: Hi! I'm a ticket booking assistant. You can ask me about available flights, prices, and booking details.")
     while True:
         user_input = input("You: ")
         if user_input.lower() in ["quit", "bye", "exit", "stop"]:
